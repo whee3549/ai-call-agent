c509e073d487ba95d61e22f78f0cda104c3db9be
whee3549 <whee3549@gmail.com>
2026-08-27 12:16:21 -0600

    edit

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ fastify.all('/incoming-call', async (request, reply) => {
 });
 
 // WebSocket route for media-stream
-fastify.register(async (fastify) => {
+fconsole.log('REGISTERING MEDIA STREAM WEBSOCKET ROUTE');astify.register(async (fastify) => {
     fastify.get('/media-stream', { websocket: true }, (connection, req) => {
         console.log('Client connected');
 
